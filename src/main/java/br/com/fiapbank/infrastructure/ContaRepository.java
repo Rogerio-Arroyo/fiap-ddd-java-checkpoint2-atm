728a176564dda442e80f54c04123b4618be47084
Rogério Arroyo <rogerio.cruz.arroyo@gmail.com>
2026-05-15 14:20:25 +0000

    Fase 5 (Infrastructure) - ContaRepository: repositório em memória para armazenamento de contas

---
+++ src/main/java/br/com/fiapbank/infrastructure/ContaRepository.java
@@ -0,0 +1,38 @@
+package br.com.fiapbank.infrastructure;
+
+import br.com.fiapbank.model.Conta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Repositório em memória para armazenamento temporário de contas.
+ * Simula a camada de persistência de dados (Infrastructure Layer - DDD).
+ */
+public class ContaRepository {
+
+    private List<Conta> contas;
+
+    public ContaRepository() {
+        this.contas = new ArrayList<>();
+    }
+
+    public void salvar(Conta conta) {
+        this.contas.add(conta);
+    }
+
+    public Conta buscarPorId(UUID id) {
+        for (Integer i = 0; i < contas.size(); i++) {
+            Conta conta = contas.get(i);
+            if (conta.getId().equals(id)) {
+                return conta;
+            }
+        }
+        return null;
+    }
+
+    public List<Conta> listarTodas() {
+        return this.contas;
+    }
+}
